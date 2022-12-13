10eb89f72b1711988e3fe47df2db55f03e380066
SayaDK <andhikaff15@gmail.com>
2022-12-13 22:03:58 +0700

    add word vocabulary to tokenizer

--- predict_sent.py
+++ predict_sent.py
@@ -10,19 +10,23 @@ max_length = 200
 trunc_type='post'
 oov_tok = "<OOV>"
 
+word_index=[]
+with open("word_index.txt",'r') as indeks:
+    for word in indeks:
+        word_index.append(word)
+tokenizer = Tokenizer(num_words = vocab_size, oov_token=oov_tok)
+tokenizer.fit_on_texts(word_index)
+
 sentiment_model = load_model(r'32_50_model.h5')
 @st.cache
 def predictor(string):
     coba_tes = []
     coba_tes.append(string)
-    tokenizer = Tokenizer(num_words = vocab_size, oov_token=oov_tok)
-    tokenizer.fit_on_texts(coba_tes)
-    word_index = tokenizer.word_index
     coba_seq = tokenizer.texts_to_sequences(coba_tes)
     coba_pad = pad_sequences(coba_seq, maxlen=max_length)
     pred = sentiment_model.predict(coba_pad)
-    if (np.round(pred)==0):
-        return("Negatif")
+    if (np.round(pred)==1):
+        return("Positif")
     else:
-        return ("Positif")
+        return ("Negatif")
  
